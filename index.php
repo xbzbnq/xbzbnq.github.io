ed3fb3dbc53d7ec371665edbdd28f8f2ce3ba9c9
candyware <95976342+candyware@users.noreply.github.com>
2021-12-26 01:18:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   file                 :  index.php
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   author               :  Muhamed Skoko | mskoko.me@gmail.com
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-include_once($_SERVER['DOCUMENT_ROOT'].'/includes.php');
-
-/* Information */
-$userHost      	= $Secure->getUserIP();
-$userOS        	= $Secure->getUserOS();
-$userBrowser   	= $Secure->getUserBrowser();
-$userAgent 		= $user_agent;
-// Request
-$Secure->saveUserInfo($userHost, $userOS, $userBrowser, $userAgent);
-// Get Image
-$gImage = $url.'/geri_brat_moj.jpg';
-
-$data = file_get_contents($gImage); header('Content-type: image/png'); echo $data; die();
+ 
+$ip = $_SERVER['REMOTE_ADDR']; // Получаем ip
+ 
+file_put_contents($ip, $ip); // записуем ip
+ 
+?>
